9ccfdc723990c08c511d9ec46103f9bd74153fe7
Lok <lok@pg95.com>
2024-05-03 12:50:09 +0100

    Implement the Serialization of a Passport User

--- server.js
+++ server.js
@@ -7,14 +7,6 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const app = express();
 
 const session = require('express-session')
-const passport = require('passport')
-const { ObjectID } = require('mongodb');
-
-fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
@@ -22,8 +14,47 @@ app.use(session({
   cookie: { secure: false }
 }));
 
+const passport = require('passport')
 passport.initialize()
 passport.session()
+
+const { ObjectID } = require('mongodb');
+
+myDB(async client => {
+  const myDataBase = await client.db('database').collection('users');
+
+  // Be sure to change the title
+  app.route('/').get((req, res) => {
+    // Change the response to render the Pug template
+    res.render('index', {
+      title: 'Connected to Database',
+      message: 'Please login'
+    });
+  });
+  // Serialization and deserialization here...
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+  
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    });
+  });
+  
+  // Be sure to add this...
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('index', { title: e, message: 'Unable to connect to database' });
+  });
+});
+// app.listen out here...
+
+fccTesting(app); //For FCC testing purposes
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 app.set('view engine','pug');
 app.set('views', './views/pug');
@@ -32,20 +63,9 @@ app.route('/').get((req, res) => {
     res.render('index',{title: 'Hello', message: 'Please log in' })
 });
 
-
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
 });
 
-passport.serializeUser((user, done) => {
-  done(null, user._id);
-});
-
-passport.deserializeUser((id, done) => {
-  //myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-    done(null, null);
-  //});
-});
 
