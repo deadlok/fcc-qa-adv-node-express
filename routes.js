e33c4dba52330c92f5ab4e79f2d4c22ea365d488
Lok <lok@pg95.com>
2024-05-09 14:08:51 +0100

    Authentication with Socket.IO

--- routes.js
+++ routes.js
@@ -4,121 +4,124 @@ const bcrypt = require('bcrypt')
 
 module.exports = function(app, myDataBase) {
 
-  // Be sure to change the title
-  app.route('/')
-  .get((req, res) => {
-    // Change the response to render the Pug template
-    res.render('index', {
-      title: 'Connected to Database',
-      message: 'Please login',
-      showLogin: true,
-      showSocialAuth: true
+    // Be sure to change the title
+    app.route('/')
+    .get((req, res) => {
+        // Change the response to render the Pug template
+        res.render('index', {
+        title: 'Connected to Database',
+        message: 'Please login',
+        showLogin: true,
+        showSocialAuth: true
+        });
     });
-  });
 
-  app.post('/login', 
-  passport.authenticate('local',{failureRedirect: '/' }),
-  (err, res) => {
-    res.redirect('/profile')
-  })
+    app.route('/login')
+    .post(
+        passport.authenticate('local',{failureRedirect: '/' }), 
+        (err, res) => {res.redirect('/profile')}    
+    )
 
-  app
-  .route('/profile')
-  .get(ensureAuthenticated, (req, res) => {
-    //console.log(req.user)
-    res.render('profile',{username: req.user.username});
-  });
+    app.route('/profile')
+    .get(
+        ensureAuthenticated, 
+        (req, res) => {
+            //console.log(req.user)
+            res.render('profile',{username: req.user.username});
+        }
+    );
 
-  app.route('/logout').get((req, res) => {
-    req.logout();
-    res.redirect('/');
-  });
-
-//   app
-//   .route('/logout')
-//   .get((req, res, next)=>{
-//     req.logout((err)=>{
-//       if (err) {
-//         return next(err)         
-//       }
-//       res.redirect('/');
-//     });
-//   })
-
-  app.route('/register')
-  .get((req, res) => {
-    res.render('index', {showRegistration: true})
-  })
-
-  app.route('/register')
-  .post(
-    //post callback 1
-    (req, res, next) => {
-    myDataBase.findOne({ username: req.body.username },
-    (err, user) => {
-      if (err) {
-        console.log(err)
-        next(err);
-      } else if (user) {
+    app.route('/logout')
+    .get((req, res) => {
+        req.logout();
         res.redirect('/');
-      } else {
-        const hash = bcrypt.hashSync(req.body.password, 12);
-        myDataBase.insertOne(
-            {username: req.body.username, password: hash},  
-            (err, doc) => {
-                if (err) {
-                    console.log(err)
-                    res.redirect('/');
-                } else {
-                    // The inserted document is held within
-                    // the ops property of the doc
-                    next(null, doc.ops[0]);
-                }
-            }
-        )
-      }
+    });
+
+    //   app
+    //   .route('/logout')
+    //   .get((req, res, next)=>{
+    //     req.logout((err)=>{
+    //       if (err) {
+    //         return next(err)         
+    //       }
+    //       res.redirect('/');
+    //     });
+    //   })
+
+    app.route('/register')
+    .get((req, res) => {
+        res.render('index', {showRegistration: true})
     })
-  }, 
-  //post callback 2
-  passport.authenticate('local', { failureRedirect: '/' }),
-  //post callback 3
-  (req, res, next) => {
-    res.redirect('/profile');
-  }
-  ) //end register post;
 
-  app.route('/auth/github')
-  .get(
-    passport.authenticate('github')
-  )
-
-  app.route('/auth/github/callback')
-  .get(
-    passport.authenticate('github',{failureRedirect: '/' }),
-    (err, res) => {
-        req.session.user_id = req.user.id;
-        res.redirect('/chat');
+    app.route('/register')
+    .post(
+        //post callback 1
+        (req, res, next) => {
+        myDataBase.findOne({ username: req.body.username },
+        (err, user) => {
+        if (err) {
+            console.log(err)
+            next(err);
+        } else if (user) {
+            res.redirect('/');
+        } else {
+            const hash = bcrypt.hashSync(req.body.password, 12);
+            myDataBase.insertOne(
+                {username: req.body.username, password: hash},  
+                (err, doc) => {
+                    if (err) {
+                        console.log(err)
+                        res.redirect('/');
+                    } else {
+                        // The inserted document is held within
+                        // the ops property of the doc
+                        next(null, doc.ops[0]);
+                    }
+                }
+            )
+        }
+        })
+    }, 
+    //post callback 2
+    passport.authenticate('local', { failureRedirect: '/' }),
+    //post callback 3
+    (req, res, next) => {
+        res.redirect('/profile');
     }
-  )
+    ) //end register post;
 
-  app.route('/chat')
-  .get(ensureAuthenticated, (req, res) => {
-    //console.log(req.user)
-    res.render('chat',{user: req.user});
-  })
+    app.route('/auth/github')
+    .get(
+        passport.authenticate('github')
+    )
 
-   app.use((req, res, next)=>{
-       res.status(404)
-         .type('text')
-         .send('Not Found')
-   });
+    app.route('/auth/github/callback')
+    .get(
+        passport.authenticate('github',{failureRedirect: '/' }),
+        (err, res) => {
+            req.session.user_id = req.user.id;
+            res.redirect('/chat');
+        }
+    )
 
-}
+    app.route('/chat')
+    .get(ensureAuthenticated, (req, res) => {
+        //console.log(req.user)
+        res.render('chat',{user: req.user});
+    })
 
-function ensureAuthenticated(req, res, next) {
-    //console.log(req)
-    if (req.isAuthenticated()) {
-      return next();
+    app.use((req, res, next)=>{
+        res.status(404)
+            .type('text')
+            .send('Not Found')
+    });
+
     }
-    res.redirect('/');
-};
+
+    function ensureAuthenticated(req, res, next) {
+        //console.log(req)
+        if (req.isAuthenticated()) {
+        return next();
+        }
+        res.redirect('/');
+    };
--- server.js
+++ server.js
@@ -9,6 +9,14 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
+const MongoStore = require('connect-mongo')(session);
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI });
+
+const passportSocketIo = require('passport.socketio')
+const cookieParser = require('cookie-parser')
+
+
 const auth = require('./auth.js')
 const routes = require('./routes.js')
 
@@ -17,9 +25,44 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
-  cookie: { secure: false }
+  cookie: { secure: false },
+  key: 'express.sid',
+  store: store
 }));
 
+// io authenticate user //
+io.use(
+  passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: 'express.sid',
+    secret: process.env.SESSION_SECRET,
+    store: store,
+    success: onAuthorizeSuccess,
+    fail: onAuthorizeFail
+  })
+);
+
+// function onAuthorizeSuccess(data, accept){
+//   console.log('successful connection to socket.io');
+//   accept();
+// }
+
+// function onAuthorizeFail(data, message, error, accept){
+//   console.log('failed connection to socket.io:', data, message);
+//   if(error)
+//     accept(new Error(message));
+// }
+
+function onAuthorizeSuccess(data, accept) {
+  console.log('successful connection to socket.io');
+  accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log('failed connection to socket.io:', message);
+  accept(null, false);
+}
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -36,16 +79,14 @@ myDB(async client => {
   io.on('connection', socket => {
     ++ currentUsers;
     io.emit('user count', currentUsers);
-    console.log('A user has connected');
+    console.log('user ' + socket.request.user.username + ' connected');
 
     socket.on('disconnect', () => {
       -- currentUsers;
       io.emit('user count', currentUsers);
-      console.log('A user has disconnected');
+      console.log('user ' + socket.request.user.username + ' disconnected');
     });
   });
-
-  
 
   auth(app, myDataBase);
   routes(app, myDataBase);
@@ -60,5 +101,3 @@ const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
 });
-
-
